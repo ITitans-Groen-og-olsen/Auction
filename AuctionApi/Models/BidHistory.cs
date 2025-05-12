45555542742b4a9bd844d763279079be109fef37
klarangrd <klara.ngaard@gmail.com>
2025-05-12 13:25:23 +0200

    testing updates

--- AuctionApi/Models/BidHistory.cs
+++ AuctionApi/Models/BidHistory.cs
@@ -2,7 +2,6 @@ namespace Models;
 
 public class BidHistory
 {
-    public string ProductId { get; set; }
     public int BidderId { get; set; }
     public decimal BidAmount { get; set; }
     public DateTime BidTime { get; set; }
