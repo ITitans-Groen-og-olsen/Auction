47f2d4510cfdf5201e4a8d850476ea65319032d3
mauaf <magn6506@gmail.com>
2025-05-24 09:46:19 +0200

    Bid method api and history on product

--- AuctionApi/Models/BidHistory.cs
+++ AuctionApi/Models/BidHistory.cs
@@ -2,8 +2,8 @@ namespace Models;
 
 public class BidHistory
 {
-    public int BidderId { get; set; }
+    public int CustomerNumber { get; set; }
     public decimal BidAmount { get; set; }
-    public DateTime BidTime { get; set; }
+    public DateTime? BidTime { get; set; }
 }
 
--- AuctionApi/Models/Product.cs
+++ AuctionApi/Models/Product.cs
@@ -16,7 +16,7 @@ public class Product
     public string? Image { get; set; }
     public DateTime EndOfAuction { get; set; } = DateTime.Now.AddDays(30);
     public int? CurrentBidder { get; set; }
-    public List<BidHistory>? BidHistory { get; set; }
+    public List<BidHistory>? BidHistory { get; set; } = new();
 
         public bool IsApproved { get; set; } = false;
 
